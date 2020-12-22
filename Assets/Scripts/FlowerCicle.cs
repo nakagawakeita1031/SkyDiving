b25d9a811e221d087c5d8b4cda4ec4795c15cb09
Nakagawakeita <nakagawakeita10@gmail.com>
2020-12-22 12:26:18 +0900

    手順１０終了

---
+++ Assets/Scripts/FlowerCicle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class FlowerCicle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //アタッチしたゲームオブジェクト(花輪)を回転させる
+        transform.DORotate(new Vector3(0, 360, 0), 5.0f, RotateMode.FastBeyond360).SetEase(Ease.Linear).SetLoops(-1, LoopType.Restart);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
