14b371de2d4d96515f6bf3610bdd769274402f23
Nakagawakeita <nakagawakeita10@gmail.com>
2021-01-26 11:41:42 +0900

    手順２３終了

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CameraController : MonoBehaviour
 {
@@ -11,12 +12,38 @@ public class CameraController : MonoBehaviour
     //キャラとカメラの距離を取得する変数
     private Vector3 offset;
 
+    //一人称カメラのCameraコンポーネント代入用
+    [SerializeField]
+    private Camera fpsCamera;
+
+    //自撮りカメラのCameraコンポーネント代入用
+    [SerializeField]
+    private Camera selfishCamera;
+
+    //カメラ制御用ボタンのButtonコンポーネント代入用
+    [SerializeField]
+    private Button btnChangeCamera;
+
+    //現在適用しているカメラの通し番号
+    private int cameraIndex = 0;
+    //Main CameraゲームオブジェクトのCameraコンポーネント代入用
+    private Camera mainCamera;
+
 
     // Start is called before the first frame update
     void Start()
     {
         //自分(カメラ)とペンギンとの相対距離を求める
         offset = transform.position - PlayerController.transform.position;
+
+        //Main Cameraゲームオブジェクトを代入する
+        mainCamera = Camera.main;
+
+        //ボタンのOnClickイベントにChangeCameraメソッドを追加
+        btnChangeCamera.onClick.AddListener(ChangeCamera);
+
+        //SetDefaultCameraメソッドを取得する
+        SetDefaultCamera();
     }
 
     // Update is called once per frame
@@ -35,5 +62,43 @@ public class CameraController : MonoBehaviour
             //自分の位置はペンギンの位置からoffsetで求めた距離を保つようにUpdateで常に監視する
             transform.position = PlayerController.transform.position + offset;
         }
+    }
+
+    /// <summary>
+    /// カメラを変更(ボタンを押すたびに呼び出される)
+    /// </summary>
+    public void ChangeCamera()
+    {
+        //現在のカメラの通し番号に応じて、次のカメラを用意して切り替える
+        switch (cameraIndex)
+        {
+            case 0:
+                cameraIndex++;
+                mainCamera.enabled = false;
+                fpsCamera.enabled = true;
+                break;
+            case 1:
+                cameraIndex++;
+                fpsCamera.enabled = false;
+                selfishCamera.enabled = true;
+                break;
+            case 2:
+                cameraIndex = 0;
+                selfishCamera.enabled = false;
+                mainCamera.enabled = true;
+                break;
+        }
+    }
+
+    /// <summary>
+    /// カメラを初期カメラ(三人称カメラ)に戻す
+    /// </summary>
+    public void SetDefaultCamera()
+    {
+        cameraIndex = 0;
+
+        mainCamera.enabled = true;
+        fpsCamera.enabled = false;
+        selfishCamera.enabled = false;
     }
 }
